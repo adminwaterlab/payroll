d38b5149b43bfeb61208a90f445223663ca9fa97
sunairphil <sunaircargophil@gmail.com>
2022-08-12 21:01:09 +0800

    edit manifest

--- service-worker.js
+++ service-worker.js
@@ -30,3 +30,7 @@ self.addEventListener('fetch', function(event){
   console.log('[Service Worker] Fetch initiated.....',event);
   event.respondWith(fetch(event.request));
 });
+
+self.addEventListener('beforeinstallprompt', (e) => {
+    deferredPrompt = e;
+});
