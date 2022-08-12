7dcd76cf8b2bbccd4481add6b3811b31cd8dff34
sunairphil <sunaircargophil@gmail.com>
2022-08-12 21:11:29 +0800

    edit manifest

--- service-worker.js
+++ service-worker.js
@@ -1,41 +1,6 @@
-'use strict';
-
-var cacheVersion = 1;
-var currentCache = {
-  offline: 'offline-cache' + cacheVersion
-};
-const offlineUrl = 'offline.html';
-
-this.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(currentCache.offline).then(function(cache) {
-      return cache.addAll();
-    })
-  );
-});
-this.addEventListener('fetch', event => {
-  // request.mode = navigate isn't supported in all browsers
-  // so include a check for Accept: text/html header.
-  if (event.request.mode === 'navigate' || (event.request.method === 'GET' && event.request.headers.get('accept').includes('text/html'))) {
-        event.respondWith(
-          fetch(event.request.url).catch(error => {
-              // Return the offline page
-              return caches.match(offlineUrl);
-          })
-    );
-  }
-  else{
-        // Respond with everything else if we can
-        event.respondWith(caches.match(event.request)
-                        .then(function (response) {
-                        return response || fetch(event.request);
-                    })
-            );
-      }
-
-/*self.addEventListener('install', function(event){
+self.addEventListener('install', function(event){
   console.log('[Service Worker] Installing Service Worker.....',event);
-});*/
+});
 
 self.addEventListener('activate', function(event){
   console.log('[Service Worker] Activating Service Worker.....',event);
